167da445b26a80ef2ae5ae76a7085082568409d9
Lucasxfff <lucasfu1994@gmail.com>
2024-11-12 00:28:11 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -9,7 +9,6 @@ contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
 
-    // Mappings to track underlying and wrapped tokens
     mapping(address => address) public underlying_tokens; // Maps underlying token to wrapped token
     mapping(address => address) public wrapped_tokens;    // Maps wrapped token to underlying token
     address[] public tokens;
@@ -32,10 +31,11 @@ contract Destination is AccessControl {
         BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
         address wrapped_token = address(newToken);
 
-        // Update mappings and tokens array
+        // Update mappings to link the underlying token and the wrapped token
         underlying_tokens[_underlying_token] = wrapped_token;
         wrapped_tokens[wrapped_token] = _underlying_token;
-        tokens.push(wrapped_token);
+
+        tokens.push(wrapped_token);  // Track the new wrapped token
 
         // Emit the Creation event
         emit Creation(_underlying_token, wrapped_token);
@@ -45,11 +45,10 @@ contract Destination is AccessControl {
 
     // Mints wrapped tokens for the specified underlying asset
     function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
-        // Ensure the token has been registered by checking the underlying token mapping
         address wrapped_token = underlying_tokens[_underlying_token];
         require(wrapped_token != address(0), "Token not registered");
 
-        // Mint the specified amount to the recipient
+        // Mint the specified amount of wrapped tokens to the recipient
         BridgeToken(wrapped_token).mint(_recipient, _amount);
 
         // Emit the Wrap event
@@ -58,11 +57,10 @@ contract Destination is AccessControl {
 
     // Burns wrapped tokens to release the underlying asset
     function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
-        // Ensure the wrapped token has a registered underlying asset
         address underlying_token = wrapped_tokens[_wrapped_token];
         require(underlying_token != address(0), "Wrapped token not registered");
 
-        // Only the token holder can burn (unwrap) their tokens
+        // Only the owner of the tokens can burn them
         BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
 
         // Emit the Unwrap event
